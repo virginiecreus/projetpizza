89450e1802b202992d9db04c625f27c75d068640
virginiecreus <creusvirginie@gmail.com>
2016-04-19 16:56:03 +0200

    Added files via upload

---
+++ formulaire commande.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="fr">
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="styles.css" media="screen" /> 
+	    <meta charset="utf-8"/>
+	    <title>TOUR DE PISE</title>
+</head>
+<div>
+<body>
+	
+ <div class="block900">
+</div>
+
+<div class="contenu titre">
+	</div>
+
+<div class="contact">
+ <form action="/contact" method="post">
+  
+   <div>
+        <label for="nom">Nom :</label>
+        <input type="text" id="nom" />
+    </div>
+    <div>
+        <label for="prenom">Prenom :</label>
+        <input type="text" id="prenom" />
+    </div>
+    <div>
+        <label for="adresse">Adresse :</label>
+        <input type="text" id="adresse" />
+    </div>
+    <div>
+        <label for="téléphone">Téléphone :</label>
+        <input type="text" id="téléphone" />
+    </div>
+    <div>
+        <label for="courriel">Courriel :</label>
+        <input type="email" id="courriel" />
+    </div>
+    <div>
+        <label for="message">Message :</label>
+        <textarea id="message"></textarea>
+    </div>
+    <div >
+    	<input type="submit" value="Envoyer"> 
+
+    </div>
+</form>
+
+ </div>   
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+</body>	    
+</div>
